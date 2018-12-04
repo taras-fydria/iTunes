f1394a42274e71d60f35f0a82fca094ed944f90d
taras-fydria <taras.fydria@gmail.com>
2018-12-04 23:43:07 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,74 @@
+function msToTime(duration) {
+	var milliseconds = parseInt((duration % 1000) / 100),
+		seconds = parseInt((duration / 1000) % 60),
+		minutes = parseInt((duration / (1000 * 60)) % 60),
+		hours = parseInt((duration / (1000 * 60 * 60)) % 24);
+
+	hours = (hours < 10) ? "0" + hours : hours;
+	minutes = (minutes < 10) ? "0" + minutes : minutes;
+	seconds = (seconds < 10) ? "0" + seconds : seconds;
+
+	return minutes + ":" + seconds;
+}
+
+
+
+;
+(function () {
+	"use strict";
+
+	const xhr = new XMLHttpRequest();
+
+
+
+	const tunesList = document.querySelector('.ba-tunes-list'),
+		tuneTmpl = document.querySelector('#tune-tmpl').innerHTML;
+	let tuneListHTML;
+	xhr.onload = function () {
+		let ajax = this;
+		const data = JSON.parse(ajax.response);
+		const tunes = data.results;
+
+		console.log(data);
+
+		tuneListHTML = '';
+
+		tunes.forEach(function (tune) {
+			// Create tune Card from tmpl and result data
+			console.log(tune.trackName);
+			let duration = msToTime(tune.trakTimeMillis);
+			let largeImg = tune.artworkUrl100.replace('100x100', '600x600');
+			tuneListHTML += tuneTmpl
+				.replace(/{{duration}}/ig, tune.duration)
+				.replace(/{{artworkUrl100}}/ig, largeImg)
+				.replace(/{{artistName}}/ig, tune.artistName)
+				.replace(/{{trackName}}/ig, tune.trackName)
+				.replace(/{{collectionName}}/ig, tune.collectionName)
+				.replace(/{{primaryGenreName}}/ig, tune.primaryGenreName)
+				.replace(/{{collectionPrice}}/ig, tune.collectionPrice)
+				.replace(/{{collectionViewUrl}}/ig, tune.collectionViewUrl);
+
+		});
+
+		tunesList.innerHTML = tuneListHTML;
+
+	};
+
+
+	const form = document.querySelector('.ba-search-form');
+	let query,
+	url;
+
+	form.addEventListener('submit', function (e) {
+		e.preventDefault();
+
+		query= form['search-query'].value;
+		url= `https://itunes.apple.com/search?term=${query}&limit=20`;
+		xhr.open('GET', url);
+		xhr.send();
+
+	})
+	console.log('hello world');
+	
+
+})();
